37972e4c71fbfda442f32c9cc0d343886ca84671
davyybs <daviferreira828@gmail.com>
2026-03-26 21:13:42 -0300

    Imports inutilizados removidos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { IonCard, IonCardHeader, IonCardTitle, IonButton, IonCardSubtitle, IonRouterLink, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonCard, IonCardHeader, IonButton, IonCardSubtitle, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonContent, IonTitle, IonToolbar, IonHeader, IonCard, IonCardHeader, IonCardTitle, IonButton, IonCardSubtitle, RouterLink],
+  imports: [IonContent, IonTitle, IonToolbar, IonHeader, IonCard, IonCardHeader, IonButton, IonCardSubtitle, RouterLink],
 })
 export class HomePage {
   constructor() {}
